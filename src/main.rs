c260de7e0b2afa986d6aa9ef59c5ac190fa54571
harapan1231 <senang.harapan@gmail.com>
2017-02-19 22:34:43 +0900

    Update main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,101 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+extern crate serde_json;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+use std::io;
+use std::path::{Path, PathBuf};
+
+use rocket::response::NamedFile;
+
+use rocket_contrib::{JSON, Value};
+use rocket::State;
+use std::collections::HashMap;
+use std::sync::Mutex;
+
+/// - Messaging
+// The type to represent the ID of a message.
+type ID = usize;
+
+// We're going to store all of the messages here. No need for a DB.
+type MessageMap = Mutex<HashMap<ID, String>>;
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    id: Option<ID>,
+    contents: String
+}
+
+/// - Routing
+///     - Root
+#[get("/")]
+fn index() -> io::Result<NamedFile> {
+    NamedFile::open("www/index.html")
+}
+
+///     - Error
+#[error(404)]
+fn not_found() -> JSON<Value> {
+    JSON(json!({
+        "status": "error",
+        "reason": "Resource was not found."
+    }))
+}
+
+///     - Static Files
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("www/").join(file)).ok()
+}
+
+///     - JSON
+#[get("/<id>", format = "application/json")]
+fn get_id(id: ID, map: State<MessageMap>) -> Option<JSON<Message>> {
+    let hashmap = map.lock().unwrap();
+    hashmap.get(&id).map(|contents| {
+        JSON(Message {
+            id: Some(id),
+            contents: contents.clone()
+        })
+    })
+}
+
+#[post("/<id>", format = "application/json", data = "<message>")]
+fn new_id(id: ID, message: JSON<Message>, map: State<MessageMap>) -> JSON<Value> {
+    let mut hashmap = map.lock().expect("map lock.");
+    if hashmap.contains_key(&id) {
+        JSON(json!({
+            "status": "error",
+            "reason": "ID exists. Try put."
+        }))
+    } else {
+        hashmap.insert(id, message.0.contents);
+        JSON(json!({ "status": "ok" }))
+    }
+}
+
+#[put("/<id>", format = "application/json", data = "<message>")]
+fn update_id(id: ID, message: JSON<Message>, map: State<MessageMap>) -> Option<JSON<Value>> {
+    let mut hashmap = map.lock().unwrap();
+    if hashmap.contains_key(&id) {
+        hashmap.insert(id, message.0.contents);
+        Some(JSON(json!({ "status": "ok" })))
+    } else {
+        None
+    }
+}
+
+/// - Main
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index, files, get_id, new_id, update_id])
+        .catch(errors![not_found])
+        .manage(Mutex::new(HashMap::<ID, String>::new()))
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket().launch();
 }
